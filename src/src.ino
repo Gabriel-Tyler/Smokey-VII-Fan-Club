a7b35cbcfa588f65a6f7cb80a81487f0be5caf53
Gabe <gabriel.tyler2003@gmail.com>
2022-02-24 12:17:56 -0500

    Create Component class

--- src/src.ino
+++ src/src.ino
@@ -1,31 +1,17 @@
-const int photoPin = 0; // analog input connected to photoresistor
-const int ledPin = 12;  // output pin
-
-int initLightVal; // initial light value read on setup
-int lightVal;     // light value read once every loop
+class Component
+{
+private:
+    const int numLEDS;
+    const int digitalPort;
+public:
+    void Set() const;
+    void Clear() const;
+};
 
 void setup()
 {
-    Serial.begin(9600);  // setup the serial monitor for light value output
-    pinMode(ledPin, OUTPUT);  // set the ledPin as output so it can be set on HIGH or LOW
-    initLightVal = analogRead(photoPin);  // read initial light value
 }
 
 void loop()
 {
-    lightVal = analogRead(sensorPin);  // read current light value
-    Serial.print("lightVal = ");  // current print light value
-    Serial.println(lightVal);
-
-    // if light value increase over a certain threshold, set ledPin on high
-    // else set on low
-    if (lightVal-50 > initialLightVal)
-    {
-        digitalWrite(ledPin, HIGH); 
-    }
-    else
-    {
-        digitalWrite(ledPin, LOW);
-    }
-    delay(20);
 }
